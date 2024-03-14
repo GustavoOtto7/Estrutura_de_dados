c9079a696183ca37b7c705116e1e46c6557e24b1
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2024-03-14 17:32:17 -0300

    up

--- Semana_02/treino.py
+++ Semana_02/treino.py
@@ -11,18 +11,56 @@ def lista_insere(lst, valor):
     novo.prox = lst
     return novo
 
-def lista_imprimi(lst):
+def lista_imprime(lst):
     atual = lst
-    while atual is not None:
+    while atual is not None:    
         print(atual.info)
         atual = atual.prox
+
+def lista_vazia(lst):
+    if lst is None:
+      #  print("A lista está vazia!")
+        return False
+    else:
+      #  print("A lista possui elementos!")
+        return True
+    
+def lista_busca(lst, valor):
+    atual = lst 
+    while atual is not None:
+        if atual.info == valor:
+            print("O valor existe!")
+            return
+        atual = atual.prox
+    print("O valor não pertence à lista!")
+
+def lista_retira(lst, valor):
+    atual = lst
+    while atual is not None:
+        if atual.info == valor:
+            aux = atual.prox
+            atual = None
+            aux.prox = None
+            print("Deu bom!")
+            print(lst)
+            return
+        atual = atual.prox
+    print("O número não existe para retirar!")
+
 
 
 def main():
     lst = lista_cria()
+    #print(lista_vazia(lst))
     lst = lista_insere(lst, 10)
     lst = lista_insere(lst, 20)
     lst = lista_insere(lst, 30)
+    lista_imprime(lst)
+    #print(lista_vazia(lst))
+    #lista_busca(lst, 20)
+    #lista_busca(lst, 22)
+    lst = lista_retira(lst, 20)
+    #lst = lista_retira(lst, 25)
+    lista_imprime(lst)
 
-    lista_imprimi(lst)
 main()
